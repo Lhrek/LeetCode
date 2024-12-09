0b24e7dc2a83973b1f10057a0b2a8743de29de20
Zhijie Gan <herujiege@gmail.com>
2024-12-09 09:58:21 +0800

    12.9

---
+++ test/Test_128_LongestConsecutive.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+public class Test_128_LongestConsecutive {
+    @Test
+    public void test0(){
+        int[] nums = {100,4,200,1,3,2};
+        assertEquals(4,LongestConsecutive_128.longestConsecutive(nums));
+    }
+
+    @Test
+    public void test1(){
+        int[] nums = {0,3,7,2,5,4,6,0,1};
+        assertEquals(9,LongestConsecutive_128.longestConsecutive(nums));
+    }
+}
